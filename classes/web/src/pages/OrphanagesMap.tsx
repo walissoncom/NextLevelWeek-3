fc4730505d55e5f7a607dbbf6cfdb86e71e60207
Walisson Rodrigues <contact@walisson.com>
2020-10-14 19:54:05 +1100

    Map page construction

--- classes/web/src/pages/OrphanagesMap.tsx
+++ classes/web/src/pages/OrphanagesMap.tsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FiPlus } from "react-icons/fi";
+
+import mapMarkerImg from "../images/map-marker.svg";
+
+import "../styles/pages/orphanages-map.css";
 
 function OrphanagesMap() {
-  return <h1>Hello World</h1>;
+  return (
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy" />
+
+          <h2>Choose an orphanage on the map</h2>
+          <p>Many kids are waiting your visit :)</p>
+        </header>
+
+        <footer>
+          <strong>Sydney</strong>
+          <span>NSW</span>
+        </footer>
+      </aside>
+
+      <div></div>
+
+      <Link to="" className="create-orphanage">
+        <FiPlus size={32} color="#FFF" />
+      </Link>
+    </div>
+  );
 }
 
 export default OrphanagesMap;
