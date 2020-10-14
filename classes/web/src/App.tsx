cd757200f68da718537c39364dfbc92e5f8931d8
Walisson Rodrigues <contact@walisson.com>
2020-10-14 19:16:12 +1100

    Pages routing set up and initial application

--- classes/web/src/App.tsx
+++ classes/web/src/App.tsx
@@ -1,33 +1,11 @@
 import React from "react";
-import { FiArrowRight } from "react-icons/fi";
 
 import "./styles/global.css";
-import "./styles/pages/landing.css";
 
-import logoImg from "./images/logo.svg";
+import Routes from "./routes";
 
 function App() {
-  return (
-    <div id="page-landing">
-      <div className="content-wrapper">
-        <img src={logoImg} alt="Happy" />
-
-        <main>
-          <h1>Take happiness to the world</h1>
-          <p>Visit orphanages and change the day of many kids.</p>
-        </main>
-
-        <div className="location">
-          <strong>Sydney</strong>
-          <span>NSW</span>
-        </div>
-
-        <a href="" className="enter-app">
-          <FiArrowRight size={26} color="rgba(0,0,0,.6)" />
-        </a>
-      </div>
-    </div>
-  );
+  return <Routes />;
 }
 
 export default App;
