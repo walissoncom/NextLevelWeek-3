03957c7423bd1992bc2d1220e892931353012e72
Walisson Rodrigues <contact@walisson.com>
2020-10-18 13:07:22 +1100

    Orphanages Detail API method created

--- classes/backend/src/routes.ts
+++ classes/backend/src/routes.ts
@@ -32,6 +32,7 @@ const routes = Router();
 // Controller
 
 routes.get("/orphanages", OrphanagesController.index);
+routes.get("/orphanages/:id", OrphanagesController.show);
 routes.post("/orphanages", OrphanagesController.create);
 
 export default routes;
