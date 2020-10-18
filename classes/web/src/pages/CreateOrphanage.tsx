0580cc7b773e38185e9239ce7393546ca46a38da
Walisson Rodrigues <contact@walisson.com>
2020-10-18 23:09:25 +1100

    Getting input form values

--- classes/web/src/pages/CreateOrphanage.tsx
+++ classes/web/src/pages/CreateOrphanage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, FormEvent } from "react";
 import { FiPlus } from "react-icons/fi";
 import { Map, Marker, TileLayer } from "react-leaflet";
 import { LeafletMouseEvent } from "leaflet";
@@ -11,6 +11,12 @@ import "../styles/pages/create-orphanage.css";
 export default function CreateOrphanage() {
   const [position, setPosition] = useState({ latitude: 0, longitude: 0 });
 
+  const [name, setName] = useState("");
+  const [about, setAbout] = useState("");
+  const [instructions, setInstructions] = useState("");
+  const [opening_hours, setOpeningHours] = useState("");
+  const [open_on_weekends, setOpenOnWeekends] = useState(true);
+
   function handleMapClick(event: LeafletMouseEvent) {
     const { lat, lng } = event.latlng;
 
@@ -19,17 +25,24 @@ export default function CreateOrphanage() {
       longitude: lng
     });
   }
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    console.log(position, name, about, instructions, opening_hours);
+  }
+
   return (
     <div id="page-create-orphanage">
       <Sidebar />
 
       <main>
-        <form className="create-orphanage-form">
+        <form onSubmit={handleSubmit} className="create-orphanage-form">
           <fieldset>
-            <legend>Dados</legend>
+            <legend>Details</legend>
 
             <Map
-              center={[-27.2092052, -49.6401092]}
+              center={[-33.8832376, 151.1983055]}
               style={{ width: "100%", height: 280 }}
               zoom={15}
               onClick={handleMapClick}
@@ -48,19 +61,28 @@ export default function CreateOrphanage() {
             </Map>
 
             <div className="input-block">
-              <label htmlFor="name">Nome</label>
-              <input id="name" />
+              <label htmlFor="name">Name</label>
+              <input
+                id="name"
+                value={name}
+                onChange={event => setName(event.target.value)}
+              />
             </div>
 
             <div className="input-block">
               <label htmlFor="about">
-                Sobre <span>Máximo de 300 caracteres</span>
+                About <span>Maximum 300 characters</span>
               </label>
-              <textarea id="name" maxLength={300} />
+              <textarea
+                id="about"
+                maxLength={300}
+                value={about}
+                onChange={event => setAbout(event.target.value)}
+              />
             </div>
 
             <div className="input-block">
-              <label htmlFor="images">Fotos</label>
+              <label htmlFor="images">Photos</label>
 
               <div className="uploaded-image"></div>
 
@@ -71,32 +93,50 @@ export default function CreateOrphanage() {
           </fieldset>
 
           <fieldset>
-            <legend>Visitação</legend>
+            <legend>Visiting</legend>
 
             <div className="input-block">
-              <label htmlFor="instructions">Instruções</label>
-              <textarea id="instructions" />
+              <label htmlFor="instructions">Instructions</label>
+              <textarea
+                id="instructions"
+                value={instructions}
+                onChange={event => setInstructions(event.target.value)}
+              />
             </div>
 
             <div className="input-block">
-              <label htmlFor="opening_hours">Nome</label>
-              <input id="opening_hours" />
+              <label htmlFor="opening_hours">Opening Hours</label>
+              <input
+                id="opening_hours"
+                value={opening_hours}
+                onChange={event => setOpeningHours(event.target.value)}
+              />
             </div>
 
             <div className="input-block">
-              <label htmlFor="open_on_weekends">Atende fim de semana</label>
+              <label htmlFor="open_on_weekends">Opens on weekends</label>
 
               <div className="button-select">
-                <button type="button" className="active">
-                  Sim
+                <button
+                  type="button"
+                  className={open_on_weekends ? "active" : ""}
+                  onClick={event => setOpenOnWeekends(true)}
+                >
+                  Yes
                 </button>
-                <button type="button">Não</button>
+                <button
+                  type="button"
+                  className={!open_on_weekends ? "active" : ""}
+                  onClick={event => setOpenOnWeekends(false)}
+                >
+                  No
+                </button>
               </div>
             </div>
           </fieldset>
 
           <button className="confirm-button" type="submit">
-            Confirmar
+            Submit
           </button>
         </form>
       </main>
