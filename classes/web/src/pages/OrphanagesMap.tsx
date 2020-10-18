fd8acc109769c035f8cb997424bafe654505aad8
Walisson Rodrigues <contact@walisson.com>
2020-10-18 19:12:40 +1100

    Adding an item to the map in OrphanagesMap

--- classes/web/src/pages/OrphanagesMap.tsx
+++ classes/web/src/pages/OrphanagesMap.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { FiPlus } from "react-icons/fi";
-import { Map, TileLayer } from "react-leaflet";
+import { FiPlus, FiArrowRight } from "react-icons/fi";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import Leaflet from "leaflet";
 
 import "leaflet/dist/leaflet.css";
 
 import mapMarkerImg from "../images/map-marker.svg";
 
 import "../styles/pages/orphanages-map.css";
+
+const mapIcon = Leaflet.icon({
+  iconUrl: mapMarkerImg,
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2]
+});
 
 function OrphanagesMap() {
   return (
@@ -27,7 +35,7 @@ function OrphanagesMap() {
       </aside>
 
       <Map
-        center={[-33.8558324, 151.1898336]}
+        center={[-33.8826531, 151.1950992]}
         zoom={15}
         style={{ width: "100%", height: "100%" }}
       >
@@ -35,9 +43,22 @@ function OrphanagesMap() {
         <TileLayer
           url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
         />
+        <Marker icon={mapIcon} position={[-33.8826531, 151.1950992]}>
+          <Popup
+            closeButton={false}
+            minWidth={240}
+            maxWidth={240}
+            className="map-popup"
+          >
+            Barnardos Australia
+            <Link to="/orphanages/11">
+              <FiArrowRight size={20} color="#FFF" />
+            </Link>
+          </Popup>
+        </Marker>
       </Map>
 
-      <Link to="" className="create-orphanage">
+      <Link to="/orphanages/create" className="create-orphanage">
         <FiPlus size={32} color="#FFF" />
       </Link>
     </div>
