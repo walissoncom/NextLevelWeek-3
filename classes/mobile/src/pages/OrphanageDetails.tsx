5766c0bd432c0b7d900003fd0c9161068b4bb562
Walisson Rodrigues <contact@walisson.com>
2020-10-24 02:24:47 +1100

    Orphanages detail page completed

--- classes/mobile/src/pages/OrphanageDetails.tsx
+++ classes/mobile/src/pages/OrphanageDetails.tsx
@@ -1,37 +1,82 @@
-import React from 'react';
-import { Image, View, ScrollView, Text, StyleSheet, Dimensions } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import React, { useEffect, useState } from 'react';
+import { Image, View, ScrollView, Text, StyleSheet, Dimensions, TouchableOpacity, Linking } from 'react-native';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import { Feather, FontAwesome } from '@expo/vector-icons';
 
 import mapMarkerImg from '../images/map-marker.png';
 import { RectButton } from 'react-native-gesture-handler';
 import { useRoute } from '@react-navigation/native';
+import api from '../services/api';
+
+interface OrphanageDetailsRouteParams {
+  id: number;
+}
+
+interface Orphanage {
+  id: number;
+  name: string;
+  latitude: number;
+  longitude: number;
+  about: string;
+  instructions: string;
+  opening_hours: string;
+  open_on_weekends: boolean;
+  images: Array<{
+    id: number;
+    url: string;
+  }>;
+}
 
 export default function OrphanageDetails() {
 
   const route = useRoute();
+  const [orphanage, setOrphanage] = useState<Orphanage>();
 
-  console.log(route.params);
+  const params = route.params as OrphanageDetailsRouteParams;
+
+  useEffect(() => {
+    api.get(`orphanages/${params.id}`).then(response => {
+      setOrphanage(response.data);
+    })
+  }, [params.id]);
+
+  if (!orphanage) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.description}>Loading...</Text>
+      </View>
+    )
+  }
+
+  function handleOpenGoogleMapRoutes() {
+    Linking.openURL(`https://www.google.com/maps/dir/?api=1&destination=${orphanage ?.latitude},${orphanage ?.longitude}`);
+  }
 
   return (
     <ScrollView style={styles.container}>
       <View style={styles.imagesContainer}>
         <ScrollView horizontal pagingEnabled>
-          <Image style={styles.image} source={{ uri: 'https://fmnova.com.br/images/noticias/safe_image.jpg' }} />
-          <Image style={styles.image} source={{ uri: 'https://fmnova.com.br/images/noticias/safe_image.jpg' }} />
-          <Image style={styles.image} source={{ uri: 'https://fmnova.com.br/images/noticias/safe_image.jpg' }} />
+          {orphanage.images.map(image => {
+            return (
+              <Image
+                key={image.id}
+                style={styles.image}
+                source={{ uri: image.url }} />
+            )
+          })}
         </ScrollView>
       </View>
 
       <View style={styles.detailsContainer}>
-        <Text style={styles.title}>Opera House</Text>
-        <Text style={styles.description}>The Sydney Opera House is a multi-venue performing arts centre at Sydney Harbour located in Sydney, New South Wales, Australia. It is one of the 20th century's most famous and distinctive buildings.</Text>
+        <Text style={styles.title}>{orphanage.name}</Text>
+        <Text style={styles.description}>{orphanage.about}</Text>
 
         <View style={styles.mapContainer}>
           <MapView
+            provider={PROVIDER_GOOGLE}
             initialRegion={{
-              latitude: -27.2092052,
-              longitude: -49.6401092,
+              latitude: orphanage.latitude,
+              longitude: orphanage.longitude,
               latitudeDelta: 0.008,
               longitudeDelta: 0.008,
             }}
@@ -39,36 +84,43 @@ export default function OrphanageDetails() {
             pitchEnabled={false}
             scrollEnabled={false}
             rotateEnabled={false}
-            style={styles.mapStyle}
-          >
+            style={styles.mapStyle}>
             <Marker
               icon={mapMarkerImg}
               coordinate={{
-                latitude: -27.2092052,
-                longitude: -49.6401092
-              }}
-            />
+                latitude: orphanage.latitude,
+                longitude: orphanage.longitude
+              }} />
           </MapView>
 
-          <View style={styles.routesContainer}>
+          <TouchableOpacity onPress={handleOpenGoogleMapRoutes} style={styles.routesContainer}>
             <Text style={styles.routesText}>Get route on Google Maps</Text>
-          </View>
+          </TouchableOpacity>
         </View>
 
         <View style={styles.separator} />
 
         <Text style={styles.title}>Instructions</Text>
-        <Text style={styles.description}>A celebration of Australian produce and wine, from the rich truffle country in the west, through tropical Queensland all the way down to cool-climate Tasmania.</Text>
+        <Text style={styles.description}>{orphanage.instructions}</Text>
 
         <View style={styles.scheduleContainer}>
           <View style={[styles.scheduleItem, styles.scheduleItemBlue]}>
             <Feather name="clock" size={40} color="#2AB5D1" />
-            <Text style={[styles.scheduleText, styles.scheduleTextBlue]}>Monday to Friday 9am to 5pm</Text>
+            <Text style={[styles.scheduleText, styles.scheduleTextBlue]}>Monday to Friday {orphanage.opening_hours}</Text>
           </View>
-          <View style={[styles.scheduleItem, styles.scheduleItemGreen]}>
-            <Feather name="info" size={40} color="#39CC83" />
-            <Text style={[styles.scheduleText, styles.scheduleTextGreen]}>We are open on weekends</Text>
-          </View>
+          {orphanage.open_on_weekends ? (
+
+            <View style={[styles.scheduleItem, styles.scheduleItemGreen]}>
+              <Feather name="info" size={40} color="#39CC83" />
+              <Text style={[styles.scheduleText, styles.scheduleTextGreen]}>We are open on weekends</Text>
+            </View>
+          ) : (
+
+              <View style={[styles.scheduleItem, styles.scheduleItemRed]}>
+                <Feather name="info" size={40} color="#FF669D" />
+                <Text style={[styles.scheduleText, styles.scheduleTextRed]}>We are not open on weekends</Text>
+              </View>
+            )}
         </View>
 
         { /*
@@ -171,6 +223,13 @@ const styles = StyleSheet.create({
     borderRadius: 20,
   },
 
+  scheduleItemRed: {
+    backgroundColor: '#FEF6F9',
+    borderWidth: 1,
+    borderColor: '#FFBCD4',
+    borderRadius: 20,
+  },
+
   scheduleText: {
     fontFamily: 'Nunito_600SemiBold',
     fontSize: 16,
@@ -184,6 +243,10 @@ const styles = StyleSheet.create({
 
   scheduleTextGreen: {
     color: '#37C77F'
+  },
+
+  scheduleTextRed: {
+    color: '#FF669D'
   },
 
   contactButton: {
